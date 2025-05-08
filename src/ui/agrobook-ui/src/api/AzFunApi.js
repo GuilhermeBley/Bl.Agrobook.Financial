9ad7cb7173617205484f22001c50f61a03753a49
Guilherme Bley <guilhermecbley@gmail.com>
2025-05-07 22:41:48 -0300

    feat(azfunapi) - fixing env

--- src/ui/agrobook-ui/src/api/AzFunApi.js
+++ src/ui/agrobook-ui/src/api/AzFunApi.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import { API_BASE_URL } from '@env';
 
 const instance = create();
 
 function create(){
-  console.debug('API URL: ' + API_BASE_URL);
+  console.debug('API URL: ' + process.env.REACT_APP_API_BASE_URL);
   return axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout: 1000 * 60 * 5 // five minutes
   });
 }
