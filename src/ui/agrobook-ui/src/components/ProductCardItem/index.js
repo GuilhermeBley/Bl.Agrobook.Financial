27a4802f3931a609cd25df1012ce5b998d4b570c
Guilherme Bley <guilhermecbley@gmail.com>
2025-05-25 21:15:42 -0300

    Create product UI #5 - handling product quantity

--- src/ui/agrobook-ui/src/components/ProductCardItem/index.js
+++ src/ui/agrobook-ui/src/components/ProductCardItem/index.js
@@ -1,15 +1,14 @@
 import { useState } from "react";
 
-function ProductCardItem({ title, quantity, description, onItemChanged = (qtty) => {} }) {
+function ProductCardItem({ title, quantity, description, onItemChanged = (qtty) => { } }) {
 
     const [cardInfo, setCardInfo] = useState({
         qttSelected: 0
     });
 
     const changeSelectedItemCount = (qtt) => {
-        if (!qtt || qtt < 0)
-        {
-            return;
+        if (!qtt || qtt < 0) {
+            qtt = 0;
         }
 
         setCardInfo(p => ({
@@ -33,17 +32,17 @@ function ProductCardItem({ title, quantity, description, onItemChanged = (qtty) 
                         <i class="fas fa-star-half-alt"></i>
                         <span class="text-muted ms-1">
                             {quantity > 0 || quantity <= 0
-                            ? <>({quantity})</>
-                            : <></>}
-                            
+                                ? <>({quantity})</>
+                                : <></>}
+
                         </span>
                     </div>
                     <p class="card-text">{description}</p>
                     <div class="d-flex justify-content-end align-items-end text-end">
                         <div class="input-group">
-                            <input className="form-control" type="number" value={cardInfo.qttSelected} style={{ maxWidth: 4 + 'rem' }} />
+                            <input className="form-control" type="number" onChange={e => changeSelectedItemCount(e.target.value)} value={cardInfo.qttSelected} style={{ maxWidth: 4 + 'rem' }} />
                             <div class="input-group-append">
-                                <button class="btn btn-outline-primary" type="button" title="Adicionar no carrinho" alt="Adicionar no carrinho">
+                                <button class="btn btn-outline-primary" onClick={e => changeSelectedItemCount(cardInfo.qttSelected + 1)} type="button" title="Adicionar no carrinho" alt="Adicionar no carrinho">
                                     <i class="bi bi-bag-plus"></i>
                                 </button>
                             </div>
