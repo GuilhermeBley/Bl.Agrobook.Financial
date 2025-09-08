a7fa7cff9b04b42d78f9933b94437fb76f577a86
Guilherme Bley <guilhermecbley@gmail.com>
2025-09-07 23:27:34 -0300

    feat(FinancialKyteApiService) - finishing API requests

--- src/server/Bl.Agrobook.Financial.Func/Services/FinancialKyteApiService.cs
+++ src/server/Bl.Agrobook.Financial.Func/Services/FinancialKyteApiService.cs
@@ -67,14 +67,14 @@ public class FinancialKyteApiService
     {
         var auth = KyteAuthenticationInfo.Create(_options.Value);
         var skip = 0;
-        const int limit = 80;
+        const int limit = 40;
         var hasMore = true;
 
         do
         {
             var request = new HttpRequestMessage(
                 HttpMethod.Get,
-                $"api/kyte-web/products/{auth.LocalId}?limit={limit}&stockStatus=&categoryId=&sort=PIN_FIRST&skip={skip}&search=&isWeb=1");
+                $"api/kyte-web/customer/{auth.LocalId}/list?sort=A_Z&filter=&limit={limit}&skip={skip}");
             AddAuthorizationHeaders(request, auth);
             using var response = await _httpClient.SendAsync(request, cancellationToken);
             response.EnsureSuccessStatusCode();
@@ -90,11 +90,84 @@ public class FinancialKyteApiService
         } while (hasMore);
     }
 
+    /// <summary>
+    /// Create a new order. It's important keep in mind that Kyte doesn't support concurrent orders creation. 
+    /// So you'll be able to just create one order by time for each account.
+    /// </summary>
     public async Task<JsonNode> CreateOrderAsync(
         GetCustomerCustomerModel customer,
-        CreateCartProductModel[] products)
+        CreateCartProductModel[] products,
+        CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(customer, nameof(customer));
+        ArgumentNullException.ThrowIfNull(products, nameof(products));
+        ArgumentOutOfRangeException.ThrowIfZero(products.Length, nameof(products));
 
+        // 
+        // In kyte web, you can just create an order by managing your cart.
+        // So first, you'll need to fill your cart and then finish.
+        // 
+        var auth = KyteAuthenticationInfo.Create(_options.Value);
+
+        // First, clear the cart to ensure no one conflict
+        using var deleteReq = new HttpRequestMessage(HttpMethod.Delete, "api/kyte-web/cart");
+        AddAuthorizationHeaders(deleteReq, auth);
+        using var deleteResp = await _httpClient.SendAsync(deleteReq, cancellationToken);
+        deleteResp.EnsureSuccessStatusCode();
+
+        // Second, Add a customer
+        using var customerReq = new HttpRequestMessage(HttpMethod.Post, $"api/kyte-web/cart/customer/{customer.Id}");
+        AddAuthorizationHeaders(customerReq, auth);
+        using var customerResp = await _httpClient.SendAsync(customerReq, cancellationToken);
+        customerResp.EnsureSuccessStatusCode();
+
+        int prodCount = 1;
+        // Then, add all the products
+        foreach (var product in products)
+        {
+            try
+            {
+                using var productReq = new HttpRequestMessage(HttpMethod.Post, $"api/kyte-web/cart/product")
+                {
+                    Content = JsonContent.Create(product, options: _jsonOptions)
+                };
+                AddAuthorizationHeaders(productReq, auth);
+                using var prodResp = await _httpClient.SendAsync(productReq, cancellationToken);
+                prodResp.EnsureSuccessStatusCode();
+                await Task.Delay(50); // small delay to avoid crawler detection
+            }
+            catch
+            {
+                throw new HttpRequestException($"Falha em adicionar produto {prodCount} em pedido.");
+            }
+            prodCount++;
+        }
+
+        using var cartReq = new HttpRequestMessage(HttpMethod.Post, $"api/kyte-web/cart");
+        AddAuthorizationHeaders(cartReq, auth);
+        using var cartResp = await _httpClient.SendAsync(cartReq, cancellationToken);
+        cartResp.EnsureSuccessStatusCode();
+        var cartDetails = await cartResp.Content.ReadFromJsonAsync<JsonNode>()
+            ?? throw new HttpRequestException("Falha em coleta de carrinho.");
+
+        // Finishing cart to complete the order, completing as order and not as sell
+        var finishingOrderObj = new FinishSaleRequestModel()
+        {
+            Payments = [
+                FinishSalePaymentRequestModel.CreateOther(products.Sum(p => p.TotalPrice), 0)
+            ],
+            PayBack = 0,
+            TotalPay = products.Sum(p => p.TotalPrice),
+        };
+        using var finishingCartReq = new HttpRequestMessage(HttpMethod.Post, $"api/kyte-web/cart/finish-sale")
+        {
+            Content = JsonContent.Create(finishingOrderObj, options: _jsonOptions)
+        };
+        AddAuthorizationHeaders(finishingCartReq, auth);
+        using var finishingCartResp = await _httpClient.SendAsync(finishingCartReq, cancellationToken);
+        finishingCartResp.EnsureSuccessStatusCode();
+
+        return cartDetails;
     }
 
     private void AddAuthorizationHeaders(HttpRequestMessage request, KyteAuthenticationInfo auth)
